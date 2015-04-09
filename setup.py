aa12bbc831542f98132c345e7cd635cc99f511ca
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-04-09 15:22:31 +0900

    [setup.py] Fix license classifier

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX',
         'Topic :: Internet :: WWW/HTTP',
     ],
